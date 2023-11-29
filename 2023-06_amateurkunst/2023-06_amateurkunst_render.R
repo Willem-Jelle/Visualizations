be4e20accf442f8ab738cd28e84b337ca4f93452
Willem-Jelle <willem-jelle@willem-jelle.nl>
2023-11-29 20:18:35 +0100

    Added project.

---
+++ 2023-06_amateurkunst/2023-06_amateurkunst_render.R
@@ -0,0 +1,14 @@
+library(quarto)
+library(webshot)
+
+# Render Quarto code to HTML ---------------------------------------------------
+
+quarto_render("2023-06_amateurkunst_code.qmd",
+              output_format = "html",
+              output_file = "2023-06_amateurkunst_html.html")
+
+# Make PNG screenshot of HTML file ---------------------------------------------
+
+webshot("2023-06_amateurkunst_html.html",
+        zoom = 1.5,
+        file = "2023-06_amateurkunst_viz.png")
